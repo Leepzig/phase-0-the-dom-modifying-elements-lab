4a0e1fd681d82af35c20094bd0dc393fa3f67cd1
andrew <neon.duncan@gmail.com>
2021-05-12 17:33:09 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("#main").remove()
+
+let newHeader = document.createElement("h1")
+newHeader.innerHTML = "ANDREW is the champion"
+newHeader.setAttribute("id", "victory")
+document.querySelector("body").appendChild(header)
